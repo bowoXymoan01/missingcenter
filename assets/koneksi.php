5f04a6325eba0e722dcb0c51202cdb15f9e6c5ec
bowoXymoan01 <andrewibowocahjtb@gmail.com>
2023-11-08 18:45:08 +0700

    bubar!

--- assets/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $host="localhost";
-    $user="missingcenter";
-    $pass="ANDRE_bowo)!01";
-    $database="missingcenter";
-    $koneksi= mysqli_connect($host,$user,$pass,$database);
-?>
