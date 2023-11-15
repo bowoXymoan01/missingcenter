32f639e9df309330c04508df6706c1b9362209a6
bowoXymoan01 <andrewibowocahjtb@gmail.com>
2023-11-15 20:09:33 +0700

    p

--- function.php
+++ function.php
@@ -1,25 +1,29 @@
 <?php
-
-function registrasi($data) {
+include_once 'mysqli-connect.php';
+function register($data) {
     global $conn;
 
-    $namalengkap = strtolower(stripslashes($data["Nama Lengkap"]));
-    $username = strtolower(stripslashes($conn, $data["Username"]));
-    $password = mysqli_real_escape_string($conn, $data["Password"]);
-    $password2 = mysqli_real_escape_string($data["Konfirmasi Password"]);
+    $nama = strtolower(stripslashes($data["nama"]));
+    $username = strtolower(stripslashes($data["username"]));
+    $password = mysqli_real_escape_string($conn, $data["password"]);
+    $password1 = mysqli_real_escape_string($conn, $data["password1"]);
 
+    $banding = mysqli_query($conn, "SELECT username FROM pengguna WHERE username ='$username'");
+    if (mysqli_fetch_assoc($banding)){
+        echo "<script>
+                alert('Username sudah digunakan')
+            </script>";
+        return 0;
+    }
 
-    if($password !== $password2) {
+    if($password !== $password1) {
         echo "<script>
                 alert('Konfirmasi Password tidak sesuai');
             </script>";
         return false;
     }
 
-    return 1;
-
-    mysqli_query($conn, "INSERT INTO pengguna VALUES('', '$username','$password','user','$namaalengkap')");
-
+    mysqli_query($conn, "INSERT INTO pengguna VALUES('', '$username','$password','user','$nama')");
     return mysqli_affected_rows($conn);
 
 }
--- register.php
+++ register.php
@@ -1,15 +1,15 @@
 <?php
 require "function.php";
 
-if(isset($_POST["Register"]) ) {
+if(isset($_POST["register"]) ) {
 
-    if(registra($_POST) > 0 ) {
+    if(register($_POST) > 0 ) {
         echo "<script>
-                alert('User Baru Berhasil Dibuat!');
+                alert('User Baru Berhasil Ditambahkan!');
             </script>";
         }
     } else {
-    echo 'mysqli_error($conn)';
+    echo mysqli_error($conn);
 }
 
 ?>
@@ -31,90 +31,38 @@ if(isset($_POST["Register"]) ) {
         <img src="img/ellipse_2.png" alt="missingcenter-logo" class="logo1" />
         <h1 class="logo">MISSING CENTER</h1>
     </header>
-    <main class="container md-10">
-        <?php
-            include_once "kon.php";
-            //skrip ini melakukan query INSERT yg menambah sebuah rekaman pada tabel pengguna.
-            if($_SERVER['REQUEST_METHOD']=='POST'){
-                $arrayError = array();//inisialisasi array error
-                
-                //apakah nama telah dimasukkan
-                if(empty($_POST['nama'])){
-                    $arrayError[]='<script type="text/javascript"git >alert("nama tidak boleh kosong");</script> ';
-                }
-                else{$nama = trim($_POST['nama']);
-                }
-
-                //apakah alamat email telah dimasukkan
-                if(empty($_POST['username'])){
-                    $arrayError[]='<script type="text/javascript">alert("username barang tidak boleh kosong");</script> ';
-                }
-                //apakah kedua password cocok
-                if(!empty($_POST['password1'])){
-                    if($_POST['password1'] != $_POST['password']){
-                        $arrayError[]='<script type="text/javascript">alert("Password yang anda masukkan tidak sama");</script> ';
-                    }
-                    else{$password = trim($_POST['password']);
-                    }
-                }
-                else{$arrayError[]='<script type="text/javascript">alert("password tidak boleh kosong");</script>' ;
-                }
-                
-                //jika sebua data terisi
-                if(empty($arrayError)){
-                    require('mysqli_connect.php');//koneksi ke database.
-                    //melakukan query
-                    $cek_data=mysqli_query($dbkoneksi, "SELECT * FROM pengguna WHERE
-                    username ='".$_POST['username']."'  ");
-                    if (mysqli_num_rows($cek_data) > 1){
-                        $q = "INSERT INTO pengguna (username,password,level,nama)
-                        VALUES('$username','$password','user','$nama')";
-                        $hasil = @mysqli_query ($dbkoneksi, $q);//menjalankan query
-                        if($hasil){
-                            //jika berhasil
-                            echo'<script type="text/javascript">alert("Data berhasil dimasukkan");</script> ';
-                            header("Location:sadata.php");
-                        }
-                    }else{
-                        echo'<script type="text/javascript">alert("Username Telah Digunakan");</script> ';
-                    }
-                    mysqli_close($dbkoneksi);//menutup koneksi
-                }
-                else{
-                    foreach ($arrayError as $psn) {//menampilkan error
-                        echo"<h11>$psn</h11><br>\n";
-                    }
-                    echo '</p><h2>Silahkan coba lagi.</h2>';
-                }
-            }
-        ?>					 
+    <main class="container md-10">				 
         <div class="row justify-content-center">
             <div class="col-md-12 box">
-                <h2 class="text-center">REGISTER</h2>
-                <form action="register.php" method="post">
+                <h2 class="text-center">DAFTAR</h2>
+                <form action="login.php" method="post">
                     <div class="mb-4">
-                        <input class="form-control" id="nama"  for="nama" 
-                        placeholder="Nama Lengkap" name="nama" type="text" required autofocus
-                        value="<?php if (isset($_POST['nama'])) echo $_POST['nama']; ?>"/>
+                        <label for="nama">
+                            <input class="form-control" id="nama"  for="nama" 
+                            placeholder="Nama Lengkap" name="nama" type="text"/>
+                        </label>
                     </div>
                     <div class="mb-4">
-                    <input class="form-control" id="username" for="username" 
-                        placeholder="Username" name="username" type="text" required autofocus
-                        value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>"/>
+                        <label for="username">
+                            <input class="form-control" id="username" for="username" 
+                            placeholder="Username" name="username" type="text" />
+                        </label>
                     </div>
                     <div class="mb-4">
-                        <input class="form-control" id="password" for="password" 
-                        placeholder="Password" name="password" type="password" required autofocus
-                        value="<?php if (isset($_POST['password'])) echo $_POST['password']; ?>"/>
+                        <label for="password">
+                            <input class="form-control" id="password" for="password" 
+                            placeholder="Password" name="password" type="password" />
+                        </label>
                     </div>
+                    <div class="mb-">
+                        <label for="password1">
+                            <input class="form-control" id="password1"  for="password1" 
+                            placeholder="konfirmasi password" name="password1" type="password" />
+                        </label>
+                        <br>
+                        <br>
                     <div class="mb-4">
-                        <input class="form-control" id="password1"  for="password1" 
-                        placeholder="Konfirmasi Password" name="password1" type="password" required autofocus
-                        value="<?php if (isset($_POST['password1'])) echo $_POST['password1']; ?>"/>
-                    </div>
-                    <div class="mb-4">
-                        <input id="submit"  class="primary fit"
-                        type="submit" name="Submit" value="Submit"/>
+                        <button type="submit" name="register">Daftarkan</button>
                     </div>
                     <div class="d-flex justify-content-center">
                         <a href="index.html">Kembali</a>
