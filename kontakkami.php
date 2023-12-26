c13e054ed10e7488182f5db2ee47e50dcfa47e2c
AprilaAditia <aymericaditia.43@gmail.com>
2023-12-26 23:00:40 +0700

    p

---
+++ kontakkami.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Silkscreen&family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/bootstrap/css/LoginAdminstyle.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/landing_page.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/style.css">
+    <title>Form Kontak</title>
+</head>
+
+<body>
+    <header>
+        <img src="img/ellipse_2.png" alt="missingcenter-logo" class="logo1" />
+        <h1 class="logo">Form Kontak</h1>
+            <nav>
+                <ul>
+                    <li><button class="btn-cta"><a href="index.html">Kembali</a></button></li> 
+                </ul>
+            </nav>
+        <div class="container">
+
+            <div class="card-container">
+                <div class="left">
+                    <div class="left-container">
+                        <h2>Tentang Kami</h2>
+                        <p>Website Missing Center adalah sebuah Website yang bertujuan untuk membantu mahasiswa dalam melaporkan dan mencari barang hilang atau temuan di lingkungan kampus Politeknik Negeri Indramayu. </p>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="right-container">
+                        <form action="">
+                            <h2 class="lg-view">Hubungi Kami</h2>
+                            <h2 class="sm-view">Hubungi Kami</h2>
+                            <input type="text" placeholder="Nama">
+                            <input type="email" placeholder="Alamat Email">
+                            <input type="text" placeholder="Nim" autocomplete="off">
+                            <input type="phone" placeholder="Telepone" autocomplete="off">
+                            <textarea rows="10" placeholder="Pesan"></textarea>
+                            <button>Kirim</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </header>
+</body>
+
+</html>
